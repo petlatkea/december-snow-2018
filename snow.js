c394e4a2a695d0cd8ca466b239d2e8b8082c4993
Peter Lind <github@peterlind.dk>
2018-12-02 19:03:24 +0100

    single snowflake falling and restarting

--- snow.js
+++ snow.js
@@ -1,9 +1,19 @@
 
 let ypos = 0;
 
+function reset(){
+  ypos = -10;
+}
+
 function move() {
   document.querySelector(".snowflake").style.top = ypos + "px";
   ypos++;
+
+  let max = document.querySelector("#nightsky").clientHeight;
+
+  if( ypos > max ) {
+    reset();
+  }
 
   requestAnimationFrame( move );
 }
