0d56621c2430d4a290d6b0c5a04f5f74dde0aacf
Peter Lind <github@peterlind.dk>
2018-12-09 02:05:45 +0100

    random xpositions 300 snowflakes

--- snow.js
+++ snow.js
@@ -1,5 +1,6 @@
 
 let ypos = [];
+let xpos = [];
 let speed = [];
 let flakes = document.querySelectorAll(".snowflake");
 
@@ -8,7 +9,8 @@ function init() {
   let start = 25;
   let increment = 50;
   for( let i = 0; i < flakes.length; i++ ) {
-    let value = start + increment*i;
+    let max = document.querySelector("#nightsky").clientWidth;
+    let value = Math.random() * max;
     flakes[i].style.left = value + "px";
 
     reset(i);
@@ -19,10 +21,12 @@ function init() {
 }
 
 function reset(i){
+  let maxX = document.querySelector("#nightsky").clientWidth;
+  xpos[i] = Math.random() * maxX;
   ypos[i] = -10;
   let min = 0.17;
-  let max = 10;
-  speed[i] = Math.random()*(max-min)+min;
+  let maxSpeed = 10;
+  speed[i] = Math.random()*(maxSpeed-min)+min;
   flakes[i].style.transform = "scale("+Math.random()+")";
 }
 
@@ -30,6 +34,7 @@ function move() {
 
   for( let i = 0; i < flakes.length; i++ ) {
     flakes[i].style.top = ypos[i] + "px";
+    flakes[i].style.left = xpos[i] + "px";
     ypos[i]+= speed[i];
 
     let max = document.querySelector("#nightsky").clientHeight;
