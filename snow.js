7d009018b96ace2cf935b40f7c1682ab238dc9d4
Peter Lind <github@peterlind.dk>
2018-12-09 14:04:41 +0100

    fix performance issues

--- snow.js
+++ snow.js
@@ -3,15 +3,17 @@ let ypos = [];
 let xpos = [];
 let speed = [];
 let flakes = document.querySelectorAll(".snowflake");
+let maxX = document.querySelector("#nightsky").clientWidth;
+let maxY = document.querySelector("#nightsky").clientHeight;
 
 function init() {
 
   let start = 25;
   let increment = 50;
   for( let i = 0; i < flakes.length; i++ ) {
-    let max = document.querySelector("#nightsky").clientWidth;
-    let value = Math.random() * max;
-    flakes[i].style.left = value + "px";
+//    let max = document.querySelector("#nightsky").clientWidth;
+//    let value = Math.random() * max;
+//    flakes[i].style.left = value + "px";
 
     reset(i);
   }
@@ -21,7 +23,6 @@ function init() {
 }
 
 function reset(i){
-  let maxX = document.querySelector("#nightsky").clientWidth;
   xpos[i] = Math.random() * maxX;
   ypos[i] = -10;
   let min = 0.17;
@@ -35,11 +36,12 @@ function move() {
   for( let i = 0; i < flakes.length; i++ ) {
     flakes[i].style.top = ypos[i] + "px";
     flakes[i].style.left = xpos[i] + "px";
+
+//    flakes[i].style.transform ="translate("+xpos[i]+"px, "+ypos[i]+"px)";
     ypos[i]+= speed[i];
 
-    let max = document.querySelector("#nightsky").clientHeight;
 
-    if( ypos[i] > max ) {
+    if( ypos[i] > maxY ) {
       reset(i);
     }
   }
